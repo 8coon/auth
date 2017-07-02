25b7db7a0b4fd744ad2cc1fa8aeea4c36153fcb3
Kaibito <gpuzzletime@icloud.com>
2017-07-02 15:40:50 +0300

    Fixed sendmail

--- src/main/java/org/minecraftshire/auth/services/EmailSender.java
+++ src/main/java/org/minecraftshire/auth/services/EmailSender.java
@@ -1,32 +1,29 @@
 package org.minecraftshire.auth.services;
 
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 
 @Service
 public class EmailSender {
 
-    private JavaMailSender sender = new JavaMailSenderImpl();
-
 
     public void sendEmailConfirmation(String email, long code, String linkHref) {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        this.setupMessage(message);
-        message.setTo(email);
-        message.setText("Код подтверждения: " + String.valueOf(code));
-
-        this.sender.send(message);
+        sentEmail(email, "Подтверждение адреса электронной почты", String.valueOf(code));
     }
 
 
-    private void setupMessage(SimpleMailMessage message) {
-        message.setFrom("no-reply@minecraftshire.ru");
+    private void sentEmail(String to, String subject, String text) {
+        String cmd = "echo \"" + text.replace("\"", "\\\"") + "\" | mail -s \"" + subject +
+                "\" -a \"From: no-reply@minecraftshire.ru\" " + to;
+
+        try {
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
