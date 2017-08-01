62a55d5398210dcd3a094cfe2623017c52397515
Kaibito <gpuzzletime@icloud.com>
2017-08-01 14:45:25 +0300

    Added service token

--- src/main/java/org/minecraftshire/auth/services/EmailSender.java
+++ src/main/java/org/minecraftshire/auth/services/EmailSender.java
@@ -4,6 +4,8 @@ package org.minecraftshire.auth.services;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 
 @Service
@@ -17,18 +19,18 @@ public class EmailSender {
 
     private void sendEmail(String to, String subject, String text) {
         String s = "" +
-            "From: github-trigger-server@minecraftshire.ru\\n" +
-            "To: " + to + "\\n" +
-            "MIME-Version: 1.0\\n" +
-            "Content-Type: text/html\\n" +
-            "Subject: " + subject + "\\n" +
-            "\\n" + text.replace("\n", "<br>");
+            "From: github-trigger-server@minecraftshire.ru\n" +
+            "To: " + to + "\n" +
+            "MIME-Version: 1.0\n" +
+            "Content-Type: text/html; charset=\\\"utf-8\\\"\n" +
+            "Subject: " + subject + "\n" +
+            "\n" + text.replace("\n", "<br>");
 
         String cmd = "echo \"" + s + "\" | sendmail -t";
         System.out.println(cmd);
 
         try {
-            Runtime.getRuntime().exec(cmd);
+            Runtime.getRuntime().exec(URLEncoder.encode(cmd,"utf-8"));
         } catch (IOException e) {
             e.printStackTrace();
         }
