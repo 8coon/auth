27c4b545af0215452eed30be280c1e74e52dab9d
Kaibito <gpuzzletime@icloud.com>
2017-08-08 17:59:12 +0300

    Rebuild

--- make-panel.js
+++ make-panel.js
@@ -2,7 +2,8 @@ const execSync = require('child_process').execSync;
 
 
 const options = {
-    cwd: `${__dirname}/assets/panel/`
+    cwd: `${__dirname}/assets/panel/`,
+    stdio: [0, 1, 2],
 };
 
 execSync('npm install', options);
