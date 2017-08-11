7aed9d52773f9b25270bdf7a093970bb401f406a
Kaibito <gpuzzletime@icloud.com>
2017-08-11 03:16:41 +0300

    Fix

--- src/main/java/org/minecraftshire/auth/aspects/ProfileAspect.java
+++ src/main/java/org/minecraftshire/auth/aspects/ProfileAspect.java
@@ -6,6 +6,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.minecraftshire.auth.utils.logging.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 
 @Aspect
@@ -17,12 +24,28 @@ public class ProfileAspect {
         Object result = pjp.proceed(pjp.getArgs());
         long end = System.currentTimeMillis();
 
+        ResponseEntity newResult = (ResponseEntity) result;
+
         if (result != null) {
             String time = String.valueOf(end - start) + "ms";
-            ((ResponseEntity) result).getHeaders().add("X-Execution-Time", time);
+            ResponseEntity oldResult = newResult;
+
+            MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+
+            for (String key: oldResult.getHeaders().keySet()) {
+                headers.put(key, oldResult.getHeaders().get(key));
+            }
+
+            headers.set("X-Execution-Time", time);
+
+            newResult = new ResponseEntity<>(
+                    oldResult.getBody(),
+                    headers,
+                    oldResult.getStatusCode()
+            );
         }
 
-        return result;
+        return newResult;
     }
 
 }
