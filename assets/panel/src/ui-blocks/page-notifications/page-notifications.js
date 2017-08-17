045f86b50884efe15b6389d5a4d399474c8b6562
Kaibito <gpuzzletime@icloud.com>
2017-08-17 20:58:40 +0300

    Added notifications

--- assets/panel/src/ui-blocks/page-notifications/page-notifications.js
+++ assets/panel/src/ui-blocks/page-notifications/page-notifications.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './page-notifications.css';
+import NoAvatar from '../../assets/no-avatar.jpg';
+
+// Utils
+import {dateDiffInDays, formatDate} from '../../utils/polyfills';
 
 // UI-Blocks
 import LayoutMain from '../layout-main/layout-main';
@@ -17,17 +21,118 @@ export default class PageNotifications extends Component {
     };
 
     static prepare() {
-        return listNotifications()
-            .then((notifications) => {
-                return {allNotifications: notifications};
+        return new Promise(resolve => {
+            listNotifications()
+                .then((notifications) => {
+                    resolve({error: null, allNotifications: notifications});
+                })
+                .catch(() => {
+                    resolve({error: 'Не удалось загрузить данные.'});
+                });
+        });
+    }
+
+    renderNotifications(blocks) {
+        const result = [];
+        const now = new Date(Date.now());
+
+        blocks.forEach(block => {
+            const days = dateDiffInDays(block[0].getCreatedAt(), now);
+            console.log(days);
+
+            if (days > 0) {
+                result.push(this.renderDate(block[0].getCreatedAt(), days, result.length));
+            }
+
+            block.forEach(notification => {
+                result.push(this.renderNotification(notification, result.length));
             });
+        });
+
+        return (<div className="notifications">{result}</div>)
+    }
+
+    renderDate(value, diff, key) {
+        let diffText;
+
+        switch(diff) {
+            case 1: diffText ='Вчера'; break;
+            case 2: diffText = 'Позавчера'; break;
+            default: diffText = formatDate(value);
+        }
+
+        return (
+            <div className="notification-delim" key={key}>
+                <span className="notification-delim__text">
+                    {diffText}
+                </span>
+            </div>
+        )
+    }
+
+    renderNotification(notification, key) {
+        return (
+            <div className={`notification ${notification.unread ? 'notification_unread' : ''}`}
+                 key={key}>
+                <div className="notification__text">
+                    <div className="notification__text__title">
+                        {notification.get('title') || 'Уведомление'}
+                    </div>
+                    <div className="notification__text_desc">
+                        {notification.get('text')}
+                    </div>
+                </div>
+                <div className="notification__picture">
+                    <div className="notification__picture__img"
+                         style={{backgroundImage: `url(${notification.get('pictureUrl') || NoAvatar})`}}/>
+                </div>
+            </div>
+        );
     }
 
     render() {
-        console.log(this.context.model.allNotifications);
+        const notifications = this.context.model.allNotifications;
+        const error = this.context.model.error;
+        const blocks = [];
+        const incorrectBlock = [];
+        let lastDate = new Date(Date.now());
+        let currentBlock = [];
+
+        if (!error) {
+            notifications.forEach(notification => {
+                if (notification.get('createdAt') === null) {
+                    incorrectBlock.push(notification);
+                    return;
+                }
+
+                const date = notification.getCreatedAt();
+
+                if (dateDiffInDays(date, lastDate) > 0) {
+                    lastDate = date;
+                    blocks.push(currentBlock);
+                    currentBlock = [];
+                }
+
+                currentBlock.push(notification);
+            });
+
+            if (currentBlock.length > 0) {
+                blocks.push(currentBlock);
+            }
+
+            if (incorrectBlock.length > 0) {
+                blocks.push(incorrectBlock);
+            }
+        }
 
         return (
             <LayoutMain title="Уведомления">
+                {error && (
+                    <div className="page__error">
+                        {error}
+                    </div>
+                )}
+                {!error && this.renderNotifications(blocks)}
             </LayoutMain>
         )
     }
--- assets/panel/src/utils/polyfills.js
+++ assets/panel/src/utils/polyfills.js
@@ -15,3 +15,61 @@ export const getEventPath = (evt) => {
 
     return path;
 };
+
+
+/**
+ * Get difference between two dates in days
+ * @param {Date} a
+ * @param {Date} b
+ * @return {number}
+ */
+export const dateDiffInDays = (a, b) => {
+    const MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / MS_PER_DAY);
+};
+
+
+/**
+ * Get localized month name by id
+ * @param {number|string} month
+ * @return {string}
+ */
+export const formatMonth = (month) => {
+    const months = [
+        'Января',
+        'Февраля',
+        'Марта',
+        'Апреля',
+        'Мая',
+        'Июня',
+        'Июля',
+        'Августа',
+        'Сентября',
+        'Октября',
+        'Ноября',
+        'Декабря',
+    ];
+
+    month = typeof month !== 'number' ? parseInt(month, 10) : month;
+    return months[month - 1];
+};
+
+/**
+ * Get localized date string
+ * @param {Date} date
+ * @return {string}
+ */
+export const formatDate = (date) => {
+    let result = date.toISOString();
+    result = result.substring(0, result.indexOf('T')).split('-');
+
+    const year = result[0];
+    const month = formatMonth(result[1]);
+    const day = parseInt(result[2], 10);
+
+    return `${day} ${month} ${year}`;
+};
